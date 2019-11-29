965ab0dc172dc7f74391e58a643dcac95f6544ba
Sridhar5879 <45038068+Sridhar5879@users.noreply.github.com>
2019-11-29 11:28:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ app = Flask(__name__)
 def order_status():
     #ssss = ""  
     if request.method == 'POST':
-      if request.form.get('sss') == "":
+      if request.form.get('sss') != "":
           #Taking the input query from user and converting it to an usable string
           userid = int(request.form.get('ui_query'))
           if userid in list(SD['ID']):
